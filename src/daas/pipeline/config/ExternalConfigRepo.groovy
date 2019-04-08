2b7f1de4327f7e6819e52f7238a2442685b506bf
RomdhanH <Houseem.Romdhane@gov.bc.ca>
2019-04-08 16:11:54 -0400

    configure application git repo

--- src/daas/pipeline/config/ExternalConfigRepo.groovy
+++ src/daas/pipeline/config/ExternalConfigRepo.groovy
@@ -4,8 +4,8 @@ package daas.pipeline.config
 
 class ExternalConfigRepo implements Serializable {
 
-	String repoUrl = "https://github.com/opticca-consulting/daas-demo-application.git" //application github URL
-	String branch = "develop"
+	String repoUrl = "git@github.com:opticca-consulting/daas-demo-application.git" //application github URL
+	String branch = "master"
 	String credentials = "id1" //we should impliment the jenkins account for github
 	String localDir = "external-config"
 
