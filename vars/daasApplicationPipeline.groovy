5d90c163fa4f15e7f5033200a7e962c8e0cb9b00
RomdhanH <Houseem.Romdhane@gov.bc.ca>
2019-04-08 16:00:12 -0400

    add closure  body to main pipeline

--- vars/daasApplicationPipeline.groovy
+++ vars/daasApplicationPipeline.groovy
@@ -1,61 +1,62 @@
 import daas.pipeline.config.PipelineConfig
 import hudson.model. *
+def call(Closure body) {
+    PipelineConfig config
 
-PipelineConfig config
-
-pipeline {
-    agent { node { label 'maven'} }
-    stages {
-        stage("Init pipeline")
-                {
-                    steps {
-                        script {
-                            initPipeline(config)
+    pipeline {
+        agent { node { label 'maven' } }
+        stages {
+            stage("Init pipeline")
+                    {
+                        steps {
+                            script {
+                                initPipeline(config)
+                            }
                         }
-                    }
 
-                }
-        stage("Init config")
-                {
-                    steps {
-                        script {
-                            initConfig(config)
+                    }
+            stage("Init config")
+                    {
+                        steps {
+                            script {
+                                initConfig(config)
+                            }
                         }
-                    }
 
-                }
-        stage("Build")
-                {
+                    }
+            stage("Build")
+                    {
 //                    when {
 //                        expression { return config.ciConfig.codeBuildStage.enable }
 //                    }
-                    steps {
-                        script{
-                            codeBuild(config)
+                        steps {
+                            script {
+                                codeBuild(config)
+                            }
                         }
                     }
-                }
-        stage("Unit & Integration tests ")
-                {
+            stage("Unit & Integration tests ")
+                    {
 //                when {
 //                    expression { return config.ciConfig.codeTestStage.enable }
 //                }
-                    steps {
-                        script {
-                            codeTest(config)
+                        steps {
+                            script {
+                                codeTest(config)
+                            }
                         }
                     }
-                }
-        stage("Dependencies check")
-                {
+            stage("Dependencies check")
+                    {
 //            when {
 //                expression{return config.ciConfig.codeSecurityStage.enable}
 //            }
-                    steps {
-                        script {
-                            dependencyCheck(config)
+                        steps {
+                            script {
+                                dependencyCheck(config)
+                            }
                         }
                     }
-                }
+        }
     }
 }
